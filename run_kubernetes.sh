66069e3ebcfa65b40cf904aa282c36073cb5b2e5
Rexben <rexben.rb@gmail.com>
2021-05-30 23:33:27 +0100

    initial commit

---
+++ run_kubernetes.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath=rexben/machine-learning
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run machine-learning --image=$dockerpath --port=80
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward machine-learning 8080:80
+
